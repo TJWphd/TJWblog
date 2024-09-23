657f405611627c74a8950d9d50da206f358601f8
“Thomas <tjwhitephd@gmail.com>
2024-09-23 16:22:04 -0700

    small edits

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,15 @@
 // listen for click on light/dark mode switch
 
+// re watch JS fundamentals from class recordings: boolean values, if statements, for loops, document.query, document.getElbyID,etc. manipulate JS after the page enters, that's after the fundamentals
+
+
+let darkMode
+
 function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
 
-// formEl.addEventListener("submit", function (event) {
-//     event.preventDefault();
-//     console.log("button clicked");
+formEl.addEventListener("", function (event) {
+    event.preventDefault();
+    console.log("button clicked");
