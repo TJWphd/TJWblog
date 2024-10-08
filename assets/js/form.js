e22278f386cb8459f260a85871ae8d5b76e2774b
“Thomas <tjwhitephd@gmail.com>
2024-10-07 18:38:38 -0700

    got night mode working but can't get back to day mode. stores post data in local storage but doesn't post it yet.

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,4 @@
 // handle form submission
-// store the blog article
-// redirect to the blog page
 
 const formEl = document.querySelector("#blogEntry");
 console.log(formEl);
@@ -10,29 +8,48 @@ formEl.addEventListener("submit", function (event) {
   console.log("button clicked");
 
   const userName = document.getElementById("userName").value;
-  console.log(userName);
+  // console.log(userName);
   const postTitle = document.getElementById("postTitle").value;
-  console.log(postTitle);
+  // console.log(postTitle);
   const content = document.getElementById("content").value;
-  console.log(content);
+  // console.log(content);
 
   const blogEntry = {
     userName: userName,
     postTitle: postTitle,
     content: content,
   };
+  console.log(blogEntry + "new entry");
 
-  // todo: WHEN I enter try to submit a form without a username, title, or content,
-  // THEN I am presented with a message that prompts me to complete the form.
+  localStorage.setItem("userName", userName);
+  localStorage.setItem("postTitle", postTitle);
+  localStorage.setItem("content", content);
+  console.log(userName);
 
-  let blogArray = JSON.parse(localStorage.getItem("blogEntries"));
-  if (!blogArray) {
-    blogArray = [];
-  }
-  console.log(blogArray);
-  blogArray.push(blogEntry);
-  localStorage.setItem("blogEntries", JSON.stringify(blogArray));
+  // displayStoredData();
 
-  window.location.href = "./blog.html";
-  // todo: get all three form entries, make them keys in an object, push that object to local storage
+  // storeLocalStorage(blogEntry);
 });
+
+// const displayStoredData = (storedData) => {
+
+// };
+
+// formEl.addEventListener("submit", blogEntry);
+// console.log(blogEntry);
+
+// store the blog article
+// redirect to the blog page
+
+// todo: WHEN I enter try to submit a form without a username, title, or content,
+// THEN I am presented with a message that prompts me to complete the form.
+
+//   // todo: get all three form entries, make them keys in an object, push that object to local storage
+// let blogArray = JSON.parse(localStorage.getItem("blogEntry"));
+// if (!blogArray) {
+//   blogArray = [];
+// }
+// console.log(blogArray);
+// blogArray.push(blogEntry);
+// localStorage.setItem("blogEntries", JSON.stringify(blogArray));
+// window.location.href = "./blog.html";
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,70 @@
-// listen for click on light/dark mode switch
-
-// re watch JS fundamentals from class recordings: boolean values, if statements, for loops, document.query, document.getElbyID,etc. manipulate JS after the page enters, that's after the fundamentals
-const darkModeButton = document.getElementById("darkMode");
-
-darkModeButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  console.log("button clicked");
-  //todo: target the page
-  // todo: add or remove the class
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+const enableDarkMode = () => {
+  document.body.classList.add("darkMode");
+  localStorage.SetItem("darkMode", "enabled");
+};
+const disableDarkMode = () => {
+  document.body.classList.remove("darkMode");
+  localStorage.SetItem("darkMode", "disabled");
+};
+darkModeToggle.addEventListener("click", () => {
+  if (darkMode !== "enabled") {
+    enableDarkMode();
+    console.log(darkMode);
+  } else {
+    disableDarkMode();
+    console.log(darkMode);
+  }
 });
+
+// listen for click on light/dark mode switch
+const themeSwitcher = document.querySelector("#toggle");
+const container = document.querySelector(".container");
+let mode = "dark";
+themeSwitcher.addEventListener("click", function () {
+  if (mode === "dark") {
+    mode = "light";
+    container.setAttribute("class", "light");
+    document.documentElement.style.setProperty("circle-color");
+  } else {
+    mode = "dark";
+    container.setAttribute("class", "dark");
+    document.documentElement.style.setProperty("circle-color");
+  }
+});
+
+// const darkModeButton = document.getElementById("darkMode");
+// darkModeButton.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   console.log("button clicked");
+//todo: target the page
+// todo: add or remove the class
+// });
+
+//code below copied from dev.to*********
+// target the button using the data attribute we added earlier
+// const button = document.querySelector("[data-theme-toggle]");
+
+// button.addEventListener("click", () => {
+//   const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+//   // update the button text
+//   const newCta =
+//     newTheme === "dark" ? "Change to light theme" : "Change to dark theme";
+//   button.innerText = newCta;
+
+//   // use an aria-label if you are omitting text on the button
+//   // and using sun/moon icons, for example
+//   button.setAttribute("aria-label", newCta);
+
+//   // update theme attribute on HTML to switch theme in CSS
+//   document.querySelector("html").setAttribute("data-theme", newTheme);
+
+//   // update in local storage
+//   localStorage.setItem("theme", newTheme);
+
+//   // update the currentThemeSetting in memory
+//   currentThemeSetting = newTheme;
+// });
+// // *************
